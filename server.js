c5194eb0e71ce3c21a57ad0f8fa771bfa947903d
Ankur Charan <ankurcharan98@gmail.com>
2018-11-12 17:59:43 +0530

    adding update person
    Signed-off-by: Ankur Charan <ankurcharan98@gmail.com>

--- server.js
+++ server.js
@@ -21,9 +21,64 @@ app.get('/', function (req, res) {
 	res.send("hi");
 })
 
-app.get('/person', function (req, res) {
+app.put('/person/:personId', function (req, res) {
 
-	console.log('hello');
+	let personId = req.params.personId;
+
+	Person.findById(personId, function (err, person) {
+		if(err) {
+
+			res.status(400).json({
+				success: false,
+				message: 'no such person exists'
+			});
+			return;
+
+		} else {
+			
+			let name = req.body.name;
+			let age = Number(req.body.age);
+			let gender = req.body.gender;
+			let contact = Number(req.body.contact);
+
+			if(name === undefined || age === undefined || gender === undefined || contact === undefined) {
+
+				res.status(400).json({
+					success: false,
+					message: 'Usage: [POST] name=NAME&age=AGE&gender=GENDER&contact=CONTACT'
+				});
+				return;
+			}
+
+			let newPerson = {
+				name,
+				age,
+				gender,
+				contact
+			};
+
+			person.set(newPerson);
+
+			person.save(function (err, updatedPerson) {
+				if(err) {
+
+					res.status(500).json({
+						success: false,
+						message: 'error updating person'
+					});
+				} else {
+
+					res.status(200).json({
+						success: true,
+						message: 'person updated'
+					});
+				}
+			});
+		}
+	})
+})
+
+app.get('/person', function (req, res) {
 
 	Person.find({}, function (err, persons) {
 		if(err) {
@@ -85,6 +140,6 @@ app.post('/person', function (req, res) {
 })
 
 app.listen(5000, function () {
-	opn('http://localhost:5000');
+	// opn('http://localhost:5000');
 	console.log('Started at http://localhost:5000');
 })
